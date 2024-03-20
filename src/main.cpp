61df85792265a2f10c4a551a12e02c17e0754e5e
stenkka <aaronstenudd@gmail.com>
2024-03-20 16:40:37 +0200

    Implement esp32->desktop communication via MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,20 @@
 
 #include <Arduino.h>
 
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+// esp32 softAP credentials
+#define ESP_SSID "ESP_DISCGOLF"
+#define ESP_PASS "alarautaan"
+
+// MQTT info
+#define MQTT_SERVER "192.168.4.2"
+
+WiFiClient espClient;
+
+PubSubClient mqttClient(espClient);
+
 mpu_meas_t measBuff[64];
 
 uint16_t acc_x_raw, acc_y_raw, acc_z_raw;
@@ -14,9 +28,25 @@ uint16_t roll, pitch, yaw;
 uint16_t elapsed_time, current_time, previous_t;
 
 void setup() {
-  Serial.begin(115200);
+   Serial.begin(115200);
+
+   Serial.print("Setting AP (Access Point)…");
+   WiFi.softAP(ESP_SSID, ESP_PASS);
+
+   delay(10000);
+
+   mqttClient.setServer(MQTT_SERVER, 1883);
+
 }
 
 void loop() {
-  Serial.write("test\n");
+   delay(2000);
+   if (!mqttClient.connected()) {
+      Serial.print("MQTT not connected, reconnecting...\n");
+      if(mqttClient.connect("mac")) Serial.print("MQTT connected.\n");
+   }
+
+   mqttClient.publish("sensors/acc", "testval");
+   
+   mqttClient.loop();
 }
