18ea5ec73ad125b0291266ab51c66e9184623147
stenkka <aaronstenudd@gmail.com>
2025-01-07 12:26:42 +0200

    Cleaning up

--- src/main.cpp
+++ src/main.cpp
@@ -20,32 +20,15 @@
 
 #define MPU_ADDR 0x68
 
-#define BUFFER_SIZE 128
-
-#define ACC_WEIGHT   1
-#define GYRO_WEIGHT  1
-
 TwoWire I2CMPU = TwoWire(0);
 Adafruit_MPU6050 mpu;
 
 TwoWire I2CDRV = TwoWire(1);
 Adafruit_DRV2605 drv;
 
-// PWM
-
-int pinMotor = 27;
-int enablePinMotor = 14;
-
 int pinBuzzer = 39;
 int enablePinBuzzer = 15;
 
-// PWM properties
-const int freqMotor = 20;
-const int pwmChannelMotor = 1;
-const int resolutionMotor = 8;
-int dutyCycleMotor = 200;
-
-const int freqBuzzer = 500;
 const int pwmChannelBuzzer = 0;
 const int resolutionBuzzer = 8;
 int dutyCycleBuzzer = 100;
@@ -73,74 +56,52 @@ WiFiClient espClient;
 
 PubSubClient mqttClient(espClient);
 
-// Data
-
-
-mpu_meas_t rawMeasBuff[BUFFER_SIZE];
-comp_filter_t filteredMeasBuff[BUFFER_SIZE];
-
-unsigned long deltaTime[BUFFER_SIZE];
-
-float accX, accY, accZ;
+volatile float accMagnitude;
+volatile float accX = 0;
+volatile float accY = 0;
+volatile float accZ = 0;
 float accXError, accYError, accZError;
 float gyroX, gyroY, gyroZ;
 float gyroXError, gyroYError, gyroZError;
-uint16_t acc_x_raw, acc_y_raw, acc_z_raw;
-uint16_t gyro_x_raw, gyro_y_raw, gyro_z_raw;
-uint16_t acc_x_err, acc_y_err, acc_z_err;
-uint16_t gyro_x_err, gyro_y_err, gyro_z_err;
-uint16_t roll, pitch, yaw;
-uint16_t delta_time, current_time, previous_t;
 
-
-   void fillMeasBuffer(mpu_meas_t* rawBuff, uint16_t sampleCount)
-   {
-
-      unsigned long elapsedTime;
-
-      // Read raw sensor data
-
-      for (int i = 0;i<sampleCount;i++)
-      {
-         // read from I2C
-         /*
-         acc_x_raw =
-         acc_y_raw =
-         acc_z_raw =
-
-         gyro_x_raw =
-         gyro_y_raw =
-         gyro_z_raw =
-         */
-
-         rawBuff[i].acc_x = acc_x_raw;
-         rawBuff[i].acc_y = acc_y_raw;
-         rawBuff[i].acc_z = acc_z_raw;
-
-         rawBuff[i].gyro_z = gyro_z_raw;
-
-         deltaTime[i] = millis() - elapsedTime;
-         elapsedTime = millis();
-
-      }
-
+void callback(char* topic, byte* message, unsigned int length) {
+   String messageTemp;
+   for (int i = 0; i < length; i++) {
+      Serial.print((char)message[i]);
+      messageTemp += (char)message[i];
    }
-
-   void fillFilteredBuffer(comp_filter_t* filter, mpu_meas_t* rawBuff, uint16_t sampleCount)
+   if (String(topic) == "game/hitresult")
    {
-
-      float filteredAngle = 0;
-
-      for (int i = 0;i<sampleCount;i++)
+      if(messageTemp == "1")
       {
-         float accAngle = getAccAngle(rawBuff[i].acc_x, rawBuff[i].acc_y, rawBuff[i].acc_z);
-         float gyroAngle = getGyroAngle(rawBuff[i].gyro_x, rawBuff[i].gyro_y, rawBuff[i].gyro_z, 1);
-
-         float filteredAngle = getCompAngle(accAngle * ACC_WEIGHT, gyroAngle * GYRO_WEIGHT, filteredAngle, GYRO_WEIGHT);
-
-         filteredMeasBuff[i].val = filteredAngle;
+         // Play high note for basket hit
+         ledcSetup(pwmChannelBuzzer, 500, resolutionBuzzer);
+         ledcAttachPin(pinBuzzer, pwmChannelBuzzer);
+         // play short double vibration
+         drv.setWaveform(0, 86);
+         drv.setWaveform(1, 86);
+         drv.setWaveform(2, 0);
+         digitalWrite(ledGreen, HIGH);
       }
-   }
+      else if(messageTemp == "0")
+      {
+         // Play low note for basket miss
+         ledcSetup(pwmChannelBuzzer, 300, resolutionBuzzer);
+         ledcAttachPin(pinBuzzer, pwmChannelBuzzer);
+         // play lower vibration
+         drv.setWaveform(0, 79);
+         drv.setWaveform(1, 79);
+         drv.setWaveform(2, 0);
+         digitalWrite(ledRed, HIGH);
+      }
+      ledcWrite(pwmChannelBuzzer, dutyCycleBuzzer);
+      drv.go();
+      delay(1500);
+      ledcDetachPin(pinBuzzer);
+      digitalWrite(ledGreen, LOW);
+      digitalWrite(ledRed, LOW);
+  }
+}
 
 void calibrateSensor()
 {
@@ -159,8 +120,7 @@ void calibrateSensor()
       I2CMPU.beginTransmission(MPU_ADDR);
       I2CMPU.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
       I2CMPU.endTransmission(false);
-      I2CMPU.requestFrom(MPU_ADDR, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-      //For a range of +-8g, we need to divide the raw values by 4096
+      I2CMPU.requestFrom(MPU_ADDR, 6, true);
       accX = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0);
       accY = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0);
       accZ = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0);
@@ -177,8 +137,7 @@ void calibrateSensor()
       I2CMPU.beginTransmission(MPU_ADDR);
       I2CMPU.write(0x43); // Start with register 0x43 (GYRO_XOUT_H)
       I2CMPU.endTransmission(false);
-      I2CMPU.requestFrom(MPU_ADDR, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-      //For a range of +-8g, we need to divide the raw values by 4096
+      I2CMPU.requestFrom(MPU_ADDR, 6, true);
       gyroX = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0);
       gyroY = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0);
       gyroZ = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0);
@@ -204,7 +163,7 @@ void setup() {
 
    I2CMPU.begin(9, 10, 100000);
 
-   // Try to initialize
+   // Try to initialize MPU
    if (!mpu.begin(MPU_ADDR, &I2CMPU)) {
       Serial.print("Failed to find MPU6050 chip");
       while (1) {
@@ -228,39 +187,28 @@ void setup() {
 
   Serial.print("Configuring acc and gyro");
 
-
-   I2CMPU.beginTransmission(MPU_ADDR);       // Start communication with MPU6050
-   I2CMPU.write(0x6B);                  // Talk to the register 6B
-   I2CMPU.write(0x00);                  // Make reset - place a 0 into the 6B register
-   I2CMPU.endTransmission(true);        //end the transmission
-
-
-   // Configure Accelerometer Sensitivity - Full Scale Range (default +/- 2g)
+   // Reset configuration
    I2CMPU.beginTransmission(MPU_ADDR);
-   I2CMPU.write(0x1C);                  //Talk to the ACCEL_CONFIG register (1C hex)
-   I2CMPU.write(0x10);                  //Set the register bits as 00010000 (+/- 8g full scale range)
+   I2CMPU.write(0x6B);
+   I2CMPU.write(0x00);
    I2CMPU.endTransmission(true);
-   // Configure Gyro Sensitivity - Full Scale Range (default +/- 250deg/s)
+
+
+   // Configure Accelerometer Sensitivity
    I2CMPU.beginTransmission(MPU_ADDR);
-   I2CMPU.write(0x1B);                   // Talk to the GYRO_CONFIG register (1B hex)
-   I2CMPU.write(0x10);                   // Set the register bits as 00010000 (1000deg/s full scale)
+   I2CMPU.write(0x1C);
+   I2CMPU.write(0x10);
+   I2CMPU.endTransmission(true);
+
+   // Configure Gyro Sensitivity
+   I2CMPU.beginTransmission(MPU_ADDR);
+   I2CMPU.write(0x1B);
+   I2CMPU.write(0x10);
    I2CMPU.endTransmission(true);
    delay(20);
 
-
    drv.selectLibrary(1);
    drv.setMode(DRV2605_MODE_INTTRIG);
-
-   // GPIO setup
-
-   // Motor
-   //ledcSetup(pwmChannelMotor, freqMotor, resolutionMotor);
-   //ledcAttachPin(pinMotor, pwmChannelMotor);
-
-
-   // Buzzer
-   //ledcSetup(pwmChannelBuzzer, freqBuzzer, resolutionBuzzer);
-   //ledcAttachPin(pinBuzzer, pwmChannelBuzzer);
 
    // Trigger
    pinMode(21, INPUT);
@@ -294,9 +242,10 @@ void setup() {
    Serial.print("Setting AP (Access Point)…");
    WiFi.softAP(ESP_SSID, ESP_PASS);
 
-   delay(1000);
+   delay(300);
 
    mqttClient.setServer(MQTT_SERVER, 1883);
+   mqttClient.setCallback(callback);
 
    calibrateSensor();
 }
@@ -309,69 +258,64 @@ void loop() {
    bool isDebugPressed = !digitalRead(1);
    bool isCalibPressed = !digitalRead(40);
 
-   accX = 0;
-   accY = 0;
-   accZ = 0;
-
    char accPrint[256] = "";
 
-   //Serial.print(isTriggerPressed);
+   if (!mqttClient.connected()) {
+         Serial.print("MQTT not connected, reconnecting...\n");
+         if(mqttClient.connect("mac"))
+         {
+            Serial.print("MQTT connected.\n");
+            mqttClient.subscribe("game/hitresult");
+         }
+         
+   }
+   mqttClient.loop();
 
    if (isTriggerPressed)
    {
       lastTriggerState = true;
       digitalWrite(ledYellow, HIGH);
+      
       // ACCELEROMETER
       I2CMPU.beginTransmission(MPU_ADDR);
       I2CMPU.write(0x3B); // Start with register 0x3B (ACCEL_XOUT_H)
       I2CMPU.endTransmission(false);
-      I2CMPU.requestFrom(MPU_ADDR, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-      //For a range of +-8g, we need to divide the raw values by 4096
+      I2CMPU.requestFrom(MPU_ADDR, 6, true);
       accX = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0) - accXError;
       accY = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0) - accYError;
       accZ = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0) - accZError;
 
       accZ -= 1; // eliminate error due to gravitation
 
-      float accMagnitude = sqrt(pow(accX, 2) + pow(accY, 2) + pow(accZ, 2));
+      accMagnitude = sqrt(pow(accX, 2) + pow(accY, 2) + pow(accZ, 2));
 
       sprintf(accPrint, "%f,%f,%f,%f\n", accX, accY, accZ, accMagnitude);
-      //Serial.print(accPrint);
 
       // GYROSCOPE
       I2CMPU.beginTransmission(MPU_ADDR);
       I2CMPU.write(0x43); // Start with register 0x43 (GYRO_XOUT_H)
       I2CMPU.endTransmission(false);
-      I2CMPU.requestFrom(MPU_ADDR, 6, true); // Read 6 registers total, each axis value is stored in 2 registers
-      //For a range of +-8g, we need to divide the raw values by 4096
+      I2CMPU.requestFrom(MPU_ADDR, 6, true);
       gyroX = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0) - gyroXError;
       gyroY = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0) - gyroYError;
       gyroZ = (int16_t)((I2CMPU.read() << 8) | I2CMPU.read()) / (4096.0) - gyroZError;
 
-      if (!mqttClient.connected()) {
-         Serial.print("MQTT not connected, reconnecting...\n");
-         if(mqttClient.connect("mac")) Serial.print("MQTT connected.\n");
-      }
-
       mqttClient.publish("sensors/acc", accPrint);
 
-      mqttClient.loop();
-   } else if (lastTriggerState && !isTriggerPressed) { // After trigger is released vibrate the moter and set yellow led low
+   }
+   else if (lastTriggerState && !isTriggerPressed) { // After trigger is released vibrate the moter and set yellow led low
       lastTriggerState = false;
-
       drv.setWaveform(0, 90);
       drv.setWaveform(1, 0);
       drv.go();
 
       digitalWrite(ledYellow, LOW);
-   } else {
+   }
+   else {
       digitalWrite(ledYellow, LOW);
    }
 
-
    delay(100);
-
-   //ledcWrite(pwmChannelBuzzer, dutyCycleBuzzer); buzzer toimii
 
    // Voltage measurement if calibration button is pressed
    if (isCalibPressed)
@@ -409,5 +353,4 @@ void loop() {
          digitalWrite(ledPower2, LOW);
          digitalWrite(ledPower3, LOW);
    }
-
 }
